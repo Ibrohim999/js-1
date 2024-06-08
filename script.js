6e4d1d68180b8a22aae504eab0118c63792aad88
Ibohim <Ibohim>
2024-06-08 11:45:35 +0500

    javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = prompt('Ismingizni kiriting')
+console.log('Ismim ' + name);
+let age = +prompt('Yoshingizni kiriting')
+console.log('Yoshim ' + age + ' da');
+let misol1 = +prompt('1-misol: 2 + 2 = ?')
+console.log('1-misolning javobi: 2 + 2 = 4  sizning javobingiz '+ misol1);
+let misol2 = +prompt('2-misol: 5 - 3 = ?')
+console.log('2-misolning javobi: 5 - 3 = 2  sizning javobingiz '+ misol2);
+let misol3 = +prompt('3-misol: 6 * 6 = ?')
+console.log('3-misolning javobi: 6 * 6 = 36  sizning javobingiz '+ misol3);
+let misol4 = +prompt('4-misol: 30 / 6 = ?')
+console.log('4-misolning javobi: 30 / 6 = 5  sizning javobingiz '+ misol4);
